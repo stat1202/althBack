e667b7a772b369c0b37c335a82a77e1dbc0bee9e
한승재(풀스택 1회차) <hseungjae1202@naver.com>
2023-11-07 02:39:43 +0000

    231107

--- 17626.py
+++ 17626.py
@@ -1,7 +1,12 @@
 import sys
-import math
 input = sys.stdin.readline
 
 N = int(input())
+dp = [0,1]
+for i in range(2, N+1):
+    min_value = 4
+    for j in range(1, int(i ** 0.5) + 1):
+        min_value = min(min_value, dp[i-(j**2)])
+    dp.append(min_value + 1)
 
-math.floor( math.pow(N, 1/2) )
+print(dp[N])
