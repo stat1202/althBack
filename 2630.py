162ffe8235b6124834f13694dc3a0b1833dbe068
한승재(풀스택 1회차) <hseungjae1202@naver.com>
2023-11-15 11:18:39 +0000

    231115

--- 2630.py
+++ 2630.py
@@ -1,20 +1,28 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 N = int( input() )
 
 papers = [ list( map(int, input().rstrip().split() ) ) for _ in range(N) ]
-queue = deque([papers])
 
-while queue:
-    paper = queue.popleft()
-    l = len(paper)
-    stack = [paper[0][0]]
-    flag = False
-    for i in range(l):
-        for j in range(1,l):
-            if paper[i][j] != stack[-1]:
-                
+answer = [0, 0]
+
+def paperSlice(x,y,N):
+    color = papers[x][y]
     
-    
+    for r in range(x, x+N):
+        for c in range(y, y+N):
+            if color != papers[r][c]:
+                paperSlice(x,y, N//2)
+                paperSlice(x,y+ N//2, N//2)
+                paperSlice(x+ N//2 ,y, N//2)
+                paperSlice(x+N//2,y+N//2, N//2)
+                return 0
+    if color == 0:
+        answer[0] += 1
+    else:
+        answer[1] += 1
+paperSlice(0,0,N)
+
+for a in answer:
+    print(a)
