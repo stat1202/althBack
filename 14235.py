f9d86f4a73c9ba7989eeb5a55e70ce6826e66f5e
한승재(풀스택 1회차) <hseungjae1202@naver.com>
2023-12-24 03:21:05 +0000

    231224

---
+++ 14235.py
@@ -0,0 +1,17 @@
+import heapq
+
+n = int(input())
+
+gifts = []
+
+for _ in range(n):
+    a = list(map(int, input().split()))
+    if a[0] == 0:
+        if len(gifts) == 0:
+            print(-1)
+        else:
+            tmp = -heapq.heappop(gifts)
+            print(tmp)
+    else:
+        for j in range(a[0]):
+            heapq.heappush(gifts, -a[j+1])
