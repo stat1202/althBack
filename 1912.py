9c40c091fb0b8c22954b893389f546b93694899a
한승재(풀스택 1회차) <hseungjae1202@naver.com>
2023-12-31 01:24:38 +0000

    231231

---
+++ 1912.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().rstrip().split() ))
+dp = [0]*n
+
+dp[0] = nums[0]
+
+for i in range(1,n):
+    dp[i] = max(dp[i-1]+nums[i], nums[i] )
+
+print(max(dp))
