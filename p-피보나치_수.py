f8c1a3c7a8bf9c85f276876cb1e1d04b7a3a2013
stat1202 <62943439+stat1202@users.noreply.github.com>
2024-03-03 10:43:11 +0900

    Create p-피보나치_수.py

---
+++ p-피보나치_수.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = [0,1]
+    if n < 2:
+        return answer[n]
+    
+    for i in range(2,n+1):
+        answer.append( (answer[i-1] + answer[i-2])%1234567 )
+    return answer[-1]
