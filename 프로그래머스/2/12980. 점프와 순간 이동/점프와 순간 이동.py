67e5472c62c2a90681f14349c54ae4a25fb7a2a7
stat1202 <62943439+stat1202@users.noreply.github.com>
2024-04-03 10:11:26 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,11 @@
+def solution(n):
+    ans = 0
+    
+    while n:
+        if n % 2 == 1:
+            ans += 1
+            n -= 1
+        else:
+            n //= 2
+            
+    return ans
