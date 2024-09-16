f46c16cf8813c25978fe41cb3da2f482887e5b56
SJ Han <62943439+stat1202@users.noreply.github.com>
2024-09-16 18:27:23 +0900

    [Silver III] Title: 숫자 야구, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,37 +1,36 @@
 from itertools import permutations
 
 N = int(input())
-user_input = []
+
+Q = []
 
 for _ in range(N):
-  user_input.append(list(map(int,  input().split(' ') )))
+  Q.append( input().split() )
 
-pers = list(permutations([1,2,3,4,5,6,7,8,9], 3))
-
-# print(user_input)
 count = 0
+for p in permutations(range(1,10), 3):
+  flag = True
 
-for per in pers:
-  flag = 0
   for i in range(N):
-    s,b = 0, 0
-    n = list(map(int, str(user_input[i][0])))
-    
+    strike = 0
+    ball = 0
     for j in range(3):
-      if per[j] == n[j]:
-        s+=1
-      else:
-        if per[j] in n:
-          b+=1
-    
-    if s != user_input[i][1] or b != user_input[i][2]:
-      flag = 0
+      if int( Q[i][0][j] ) == p[j]:
+        strike += 1
+        continue
+
+      if int( Q[i][0][j] ) in p:
+        ball += 1
+
+    if strike != int( Q[i][1] ) :
+      flag = False
       break
-    else:
-      flag = 1
-  if flag == 1:
+    if ball != int( Q[i][2] ):
+      flag = False
+      break
+    
+
+  if flag:
     count += 1
 
 print(count)
-
-
