ec41eed59cb908ec1f5f191f56ddb38c551bb29c
SJ Han <62943439+stat1202@users.noreply.github.com>
2024-10-03 15:32:10 +0900

    [level 2] Title: 숫자 블록, Time: 6974.65 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.py
@@ -3,14 +3,10 @@ def solution(begin, end):
     
     for i in range(begin, end + 1):
         n = 0
-        if i != 1:
-            n = 1
-            for j in range(2, int(i ** 0.5) + 1):
-                if i % j == 0:
-                    if i // j > 10000000:
-                        n = max(n, j)
-                    else:
-                        n = max([n, i//j, j])
+        
+        for j in range(1, min(i // 2 + 1, 10000000) ):
+            if i % j == 0:
+                n = j
         answer.append(n)
     
     return answer
