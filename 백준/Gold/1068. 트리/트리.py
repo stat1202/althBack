12fdd28f4c3588f376c009ee736b0b3eda899233
SJ Han <62943439+stat1202@users.noreply.github.com>
2024-10-03 14:00:33 +0900

    [Gold V] Title: 트리, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,36 +1,17 @@
 N = int(input())
-parent = list(map(int, input().split()))
-R = int(input())
+parent = list( map(int, input().split(" ")) )
+D = int(input())
 
+def dfs(n):
+  parent[n] = -10
+  for i in range(N):
+    if n == parent[i]:
+      dfs(i)
+
+dfs(D)
+
+answer = 0
 for i in range(N):
-  if parent[i] == -1:
-    root = i
-    break
-
-black = [0] * N
-for i in range(N):
-  u = i
-  while True:
-    if u == R:
-      black[i] = 1
-      break
-    if u == root:
-      break
-    u = parent[u]
-
-red = [0] * N
-for i in range(N):
-  if black[i] == 1:
-    continue
-
-  if i == root:
-    continue
-
-  red[parent[i]] = 1
-
-count = 0
-for i in range(N):
-  if black[i] == 1 or red[i] == 1:
-    continue
-  count += 1
-print(count)
+  if parent[i] != -10 and i not in parent:
+    answer += 1
+print(answer)
