aa561b02f6d3c88351281b0f5b8952c2e510eeb9
SJ Han <62943439+stat1202@users.noreply.github.com>
2024-12-09 11:44:16 +0900

    [Gold III] Title: 과제, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -1,39 +1,17 @@
-import heapq
-
 N = int(input())
 
-day = 0
-assignments = []
-score = 0
+H = [list(map(int, input().split())) for _ in range(N)]
 
-for _ in range(N):
-  d, w =  map(int, input().split())
-  day = max(d, day)
-  heapq.heappush(assignments, (-d, -w) )
+H.sort(key = lambda x : x[1], reverse = True)
 
-for i in range(day, 0, -1):
-  tmp = []
-  cand = []
-  while assignments:
-    d, w = heapq.heappop(assignments)
-    
-    if -d >= i:
-      cand.append( (-w,-d) )
-    else:
-      tmp.append((d,w))
-  
-  cand.sort()
-  
-  if cand:
-    score += cand.pop()[0]
+S = [False] * 1001
 
-  for t in tmp:
-    heapq.heappush(assignments, t)
-  
-  for c in cand:
-    heapq.heappush(assignments, (-c[1], -c[0]))
+answer = 0
+for i in range(N):
+  for j in range(H[i][0], 0, -1):
+    if not S[j]:
+      S[j] = True
+      answer += H[i][1]
+      break
 
-print(score)
-
-
-
+print(answer)
